caee3672fb138e0bf65f3b332366196d9d8e5ea9
DieguitoNC <diegocatalann@gmail.com>
2025-01-26 00:16:39 -0300

    aprendendo_js

---
+++ Objetos/scripts.js
@@ -0,0 +1,10 @@
+let message = "Estou estudando os fundamentos do Javascript"
+
+// Obtem a posicao da palavra
+console.log(message.indexOf("Javascript"))
+
+//verificar se existe a palavra na string
+console.log(message.includes("Javascript"))
+console.log(message.toLowerCase().includes("javascript"))
+
+console.log(message.includes("html"))
