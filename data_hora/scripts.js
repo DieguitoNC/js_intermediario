214f5bf9cac57c8b91da7590cb81b70fa4fc6966
DieguitoNC <diegocatalann@gmail.com>
2025-01-27 11:03:28 -0300

    estudo_data_hora_js

---
+++ data_hora/scripts.js
@@ -0,0 +1,6 @@
+const dateWithTimezone = new Date("2025-01-27T13:56:08.514-03:00")
+
+//console.log(dateWithTimezone.toISOString())
+
+console.log(new Date().toLocaleString())
+console.log(dateWithTimezone.toLocaleString())
